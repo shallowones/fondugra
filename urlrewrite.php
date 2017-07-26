cb7b6d55bd0153c2f8d28e1feb0775aed6a2b37d
Vladimir <spasennikov-vs@yandex.ru>
2017-07-26 20:06:00 +0500

    Выведен список мероприятий

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,6 +1,12 @@
 <?
 $arUrlRewrite = array(
 	array(
+		"CONDITION" => "#^/events/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/events/index.php",
+	),
+	array(
 		"CONDITION" => "#^/news/#",
 		"RULE" => "",
 		"ID" => "bitrix:news",
