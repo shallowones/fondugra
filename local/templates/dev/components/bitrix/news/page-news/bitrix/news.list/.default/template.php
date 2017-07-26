65a532da63d2ca70132a6f462a59557d9f75aee1
Vladimir <spasennikov-vs@yandex.ru>
2017-07-26 19:13:36 +0500

    Добавлена возможность делать новость важной

--- local/templates/dev/components/bitrix/news/page-news/bitrix/news.list/.default/template.php
+++ local/templates/dev/components/bitrix/news/page-news/bitrix/news.list/.default/template.php
@@ -23,32 +23,34 @@ $this->setFrameMode(true);
             CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"),
             array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         ?>
-    <p class="news-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
-            <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><img
-                        class="preview_picture"
-                        border="0"
-                        src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                        alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
-                        title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
-                        style="float:left"
-                /></a>
-        <? endif ?>
-        <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
-            <span class="news-date-time"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
-        <? endif ?>
-        <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
-            <a href="<? echo $arItem["DETAIL_PAGE_URL"] ?>"><b><? echo $arItem["NAME"] ?></b></a><br/>
-        <? endif; ?>
-        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
-            <? echo $arItem["PREVIEW_TEXT"]; ?>
-        <? else: ?>
-            <? echo TruncateText(strip_tags($arItem['FIELDS']["DETAIL_TEXT"]), 200); ?>
-        <? endif; ?>
-        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
-            <div style="clear:both"></div>
-        <? endif ?>
-        </p>
+        <div class="news-item <? if (!empty($arItem['PROPERTIES']['important']['VALUE'])) {
+            echo 'news-important';
+        } ?>" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+            <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
+                <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><img
+                            class="preview_picture"
+                            border="0"
+                            src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
+                            alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
+                            title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
+                            style="float:left"
+                    /></a>
+            <? endif ?>
+            <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
+                <span class="news-date-time"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
+            <? endif ?>
+            <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
+                <a href="<? echo $arItem["DETAIL_PAGE_URL"] ?>"><b><? echo $arItem["NAME"] ?></b></a><br/>
+            <? endif; ?>
+            <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                <? echo $arItem["PREVIEW_TEXT"]; ?>
+            <? else: ?>
+                <? echo TruncateText(strip_tags($arItem['FIELDS']["DETAIL_TEXT"]), 200); ?>
+            <? endif; ?>
+            <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
+                <div style="clear:both"></div>
+            <? endif ?>
+        </div>
     <? endforeach; ?>
     <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
         <br/><?= $arResult["NAV_STRING"] ?>
