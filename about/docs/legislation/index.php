dc4423ec441c7ac32a9e4dbd5982e8e22828a278
Your Name <you@example.com>
2017-08-03 10:35:48 +0500

    Контентные изменения на дев-сервере

---
+++ about/docs/legislation/index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Законодательство в сфере ГЧП и концессии");
+?>
+
+Text here....
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
