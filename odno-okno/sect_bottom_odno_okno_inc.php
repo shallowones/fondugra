7cc381e62341a150a69d45f0ac1822c0b9e0c209
bpa <bpa@ugraweb.ru>
2017-09-15 13:05:24 +0500

    Исправлен баг с видео в разделе /odno-okno

--- odno-okno/sect_bottom_odno_okno_inc.php
+++ odno-okno/sect_bottom_odno_okno_inc.php
@@ -2,20 +2,28 @@
     <div class="video-block__line-left"></div>
     <div class="container inner-detail">
         <h2 class="h2">Как работает одно окно?</h2>
-        <?if($_SESSION["slow_vision"] == "N"):?>
-        <div class="video">
-            <iframe width="850" height="480" src="https://www.youtube.com/embed/FLG34ETTBmM"  frameborder="0" allowfullscreen></iframe>
-        </div>
-        <?else:?>
-            <a class="file" href="https://www.youtube.com/embed/FLG34ETTBmM">Сопровождение инвестиционных проектов по принципу Одного окна</a>
-        <?endif;?>
+        <? if ($_SESSION["slow_vision"] !== 'Y'): ?>
+            <div class="video">
+                <iframe width="850" height="480" src="https://www.youtube.com/embed/FLG34ETTBmM" frameborder="0"
+                        allowfullscreen></iframe>
+            </div>
+        <? else: ?>
+            <a class="file" href="https://www.youtube.com/embed/FLG34ETTBmM" target="_blank">
+                Сопровождение инвестиционных проектов по принципу Одного окна
+            </a>
+        <? endif; ?>
     </div>
     <div class="video-block__line-right"></div>
 </div>
 <div class="container inner-detail">
     <h2 class="h2">Важно</h2>
-    <a class="file" href="/upload/reglament_po_soprovozhdeniy_invest_proectov.docx">Регламент по сопровождению инвестиционных проектов в Ханты-Мансийском автономном округе – Югре</a>
-    <a class="file" href="/upload/zayavlenie_110-p_ot_14.05.2015.docx">Заявление на оказание информационно – консультационного и организационного сопровождения инвестиционного проекта в Ханты-Мансийском автономном округе – Югре</a>
-    <a class="file" href="/upload/forma_biznes_plana_invest_proecta_110p_14.05.2015.docx">Форма бизнес-плана инвестиционного проекта</a>
-    <a class="file" href="/upload/medialibrary/77e/jpg2pdf.pdf">Порядок сопровождения инвестиционных проектов в Ханты-Мансийском автономном округе – Югре</a>
+    <a class="file" href="/upload/reglament_po_soprovozhdeniy_invest_proectov.docx">Регламент по сопровождению
+        инвестиционных проектов в Ханты-Мансийском автономном округе – Югре</a>
+    <a class="file" href="/upload/zayavlenie_110-p_ot_14.05.2015.docx">Заявление на оказание информационно –
+        консультационного и организационного сопровождения инвестиционного проекта в Ханты-Мансийском автономном округе
+        – Югре</a>
+    <a class="file" href="/upload/forma_biznes_plana_invest_proecta_110p_14.05.2015.docx">Форма бизнес-плана
+        инвестиционного проекта</a>
+    <a class="file" href="/upload/medialibrary/77e/jpg2pdf.pdf">Порядок сопровождения инвестиционных проектов в
+        Ханты-Мансийском автономном округе – Югре</a>
 </div>
